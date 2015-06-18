b4b12a86a91688bbd6d8def84c057ee4ea6fe743
hiroki-labmac <hiroki2101@gmail.com>
2015-06-18 15:38:39 +0900

    import文の練習

--- src/keisan/Calc.java
+++ src/keisan/Calc.java
@@ -1,12 +1,15 @@
 package keisan;
 
+//importでパッケージの宣言が不要になる
+import keisansub.CalcLogic;	//keisansubはソースファイル名,CalcLogicはクラス名
+
 public class Calc {
 
 	//実行するのはmainメソッドが含まれているクラスの名前
 	public static void main(String[] arg){
 		int a =10; int b=2;
-		int total = keisansub.CalcLogic.tasu(a,b);	//クラスが書かれているソースファイル名.クラス名（引数）;
-		int delta = keisansub.CalcLogic.hiku(a, b);
+		int total = CalcLogic.tasu(a,b);	//パッケージ名.ソースファイル名.クラス名（引数）;
+		int delta = CalcLogic.hiku(a, b);	//パッケージが異なれば同じクラス名を使ってもOK
 		System.out.println("足したら" + total +"、引いたら" + delta);
 	}
 }
